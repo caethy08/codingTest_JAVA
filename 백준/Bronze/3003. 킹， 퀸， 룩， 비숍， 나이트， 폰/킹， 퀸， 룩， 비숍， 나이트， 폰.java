93533a58126a11cdbd38f4d8aa0ea2fa274e7fc5
caethy08 <167155182+caethy08@users.noreply.github.com>
2024-08-22 09:57:39 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 116 ms, Memory: 16160 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		//배열의 입력
+		int[] origin = new int[] {1,1,2,2,2,8};
+		int[] my = new int [6];
+		
+		for (int i = 0; i < my.length; i++) {
+			my[i] =  Integer.parseInt(st.nextToken());
+		}
+		br.close();		
+		
+		//배열의 각 값의 자리 비교 후 연산
+		int[] answer = new int[6]; 
+		for (int i = 0; i < my.length; i++) {
+			if (origin[i] != my[i]) {
+				answer[i] = origin[i] - my[i];
+			}else {
+				answer[i] = 0;
+			}
+			
+			bw.write(String.valueOf(answer[i] + " "));
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+}
